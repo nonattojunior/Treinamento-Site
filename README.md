ae9a9d80b21408e59a66a90ce48a827ddd34f2d8
nonattojunior <jrdias.dev@gmail.com>
2022-07-01 18:26:43 -0300

    finalização html e css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Treinamento-Site
+ - Criação de Site com o objetivo de mostrar produtos e informações deles.
+ Feito em HTML e CSS.
+ Posteriormente será feito modal para poder abrir as informçãoes dos produtos em cards.
---
+++ main.js
@@ -0,0 +1,20 @@
+/*function imc(peso, altura) {
+  let imc = peso / altura ** 2;
+  return imc;
+}
+
+imc(80, 1.8)*/
+
+/*function corFavorita(cor) {
+  if (cor === 'azul') {
+    return 'Eu gosto do céu'
+  } else if (cor === 'verde') {
+    return 'Eu gosto de Mar'
+  } else {
+    'Não gosto de cores'
+  }
+}
+
+corFavorita('verde')*/
+
+
